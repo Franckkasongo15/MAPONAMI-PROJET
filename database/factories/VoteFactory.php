4cecebc898774f727be479dc34e616aa1cc58ae6
AristoteKABELU <20kk090@esisalama.org>
2023-10-25 00:37:23 +0200

    seeders and factories

---
+++ database/factories/VoteFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class VoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'prenom' => $this->faker->name(),
+            'pseudo' => $this->faker->name(),
+            'numero' => $this->faker->phoneNumber(),
+            'genre' => 'm|f',
+            'photo' => '',
+            'vote_number' => $this->faker->numberBetween(1, 20),
+            'motivation' => $this->faker->sentences(4, true),
+            'promotion' => $this->faker->randomLetter().$this->faker->numberBetween(1,4),
+            'slogan' => $this->faker->sentences(4, true),
+            'email' => $this->faker->email()
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Vote;
+use App\Models\Voted;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::factory()->create([
+            'email'=> 'aristote@gmail.com'
+        ]);
+       Vote::factory(20)->create();
+       Voted::factory(20)->create();
     }
 }
